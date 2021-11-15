8598c20ac2a7668bdcf9a3580483ca4950d13a73
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-15 21:01:38 +0100

    Add files via upload
    terminado

---
+++ serviceconfig/src/main/java/com/nttdata/bootcamp/config/ServiceconfigApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.bootcamp.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ServiceconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceconfigApplication.class, args);
+	}
+
+}
---
+++ serviceconfig/src/test/java/com/nttdata/bootcamp/config/ServiceconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
