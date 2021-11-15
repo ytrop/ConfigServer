a5101f4f55e264e6db74d3a1766221953e7875ed
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-15 14:09:45 +0100

    Add files via upload
    commit

---
+++ configServ/src/test/java/com/example/configServ/ConfigServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configServ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
